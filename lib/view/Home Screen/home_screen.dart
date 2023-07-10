c59fcd2f7b74f181b1316f4cf2bd2b4bf1238139
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-07-10 08:14:44 +0500

    build profile screen

--- lib/view/Home Screen/home_screen.dart
+++ lib/view/Home Screen/home_screen.dart
@@ -16,7 +16,49 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Profile'),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('images/download.jpeg'),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Name'),
+              subtitle: Text('Shahab Mustafa'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Father Name'),
+              subtitle: Text('Ghulam Mustafa'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone Number'),
+              subtitle: Text('+92311XXXXXXXX'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.location_pin),
+              title: Text('Location'),
+              subtitle: Text('Peshawar Pakistan'),
+            ),
+          ),
+        ],
       ),
     );
   }
