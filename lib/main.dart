09b632e531c91ca28a1e8263bc886ae8e1f62ac9
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-09-14 17:41:26 +0500

    this page i initializ firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
-import 'package:firebase_practis/view/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_practis/view/auth/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   runApp(const MyApp());
 }
 
