e08f254f57c3ec00403d69907acd23c864fc2c0e
Shahab Mustafa <shahabmustafa57@gmail.com>
2023-09-18 09:33:04 +0500

    add home screen

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,30 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        automaticallyImplyLeading: false,
+        actions: [
+          TextButton(onPressed: (){
+            FirebaseAuth.instance.signOut().then((value){
+              Navigator.pop(context);
+            });
+          }, child: Text("sign Out"))
+        ],
+      ),
+    );
+  }
+}
+
